a4b3285197a50e5273297908dbfd372c232269a9
MichaelHiszpanski <michal.hiszpanski@thoc.dev>
2024-05-15 18:45:56 +0100

    menu_screen

--- lib/space_galaxy_shooter/screens/menu/menu_screen.dart
+++ lib/space_galaxy_shooter/screens/menu/menu_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:space_galaxy_shooter/space_galaxy_shooter/components/ui/custom_floating_button/custom_floating_button.dart';
+import 'package:space_galaxy_shooter/space_galaxy_shooter/game_utils/game_config/game_configuration.dart';
 import 'package:space_galaxy_shooter/space_galaxy_shooter/screens/game_play/game_play_screen.dart';
 import 'package:space_galaxy_shooter/space_galaxy_shooter/screens/game_results/game_results.dart';
 
@@ -28,17 +30,34 @@ class _MenuScreenState extends State<MenuScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-          child: Column(
-        children: [
-          FloatingActionButton(
-            onPressed: () => _navigateToPlayGame(context),
-            child: const Text('Start Game'),
-          ),
-          FloatingActionButton(
-            onPressed: () => _navigateToResultsScreen(context),
-            child: const Text('Results'),
-          ),
-        ],
+          child: Center(
+        child: Column(
+          children: [
+            Container(
+                width: Config.gameScreenSize,
+                height: Config.gameScreenSize,
+                decoration: const BoxDecoration(
+                    image: DecorationImage(
+                        image: AssetImage("assets/images/menu_screen.png"),
+                        fit: BoxFit.cover)),
+                child: Column(
+                  children: [
+                    const SizedBox(
+                      height: 200.0,
+                    ),
+                    CustomFloatingButton(
+                        onPressed: () => _navigateToPlayGame(context),
+                        buttonName: 'Start Game'),
+                    const SizedBox(
+                      height: 400.0,
+                    ),
+                    CustomFloatingButton(
+                        onPressed: () => _navigateToResultsScreen(context),
+                        buttonName: 'Results')
+                  ],
+                ))
+          ],
+        ),
       )),
     );
   }
