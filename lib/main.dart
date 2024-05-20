a5f76ef06572efd7ae25f3ab22a7ba8bab1cc489
MichaelHiszpanski <michal.hiszpanski@thoc.dev>
2024-05-20 10:01:17 +0100

    flutter riverpods aded

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:space_galaxy_shooter/space_galaxy_shooter/game_main/game_main.dart';
 import 'package:space_galaxy_shooter/space_galaxy_shooter/screens/game_play/game_play_screen.dart';
 import 'package:space_galaxy_shooter/space_galaxy_shooter/screens/login/login_screen.dart';
 import 'package:space_galaxy_shooter/space_galaxy_shooter/screens/menu/menu_screen.dart';
 import 'package:space_galaxy_shooter/space_galaxy_shooter/screens/splash/splash_screen.dart';
 
+final userLoginProvider = StateProvider<dynamic>((ref) => null);
+final allUsersDataProvider = StateProvider<dynamic>((ref) => null);
 void main() {
-  runApp(const MainApp());
+  runApp(
+    const ProviderScope(
+      child: MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
